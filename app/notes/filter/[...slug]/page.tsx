e042832fd23723b3a17e69bea758905a9f08f730
Volodymyr Vasyl'ev <vasyliev.official@gmail.com>
2025-08-13 21:50:22 +0300

    +

--- app/notes/filter/[...slug]/page.tsx
+++ app/notes/filter/[...slug]/page.tsx
@@ -5,11 +5,9 @@ type Props = {
   params: Promise<{ slug: string[] }>;
 };
 
-
-export default async function NotesPage({params}: Props) {
+export default async function NotesPage({ params }: Props) {
   const { slug } = await params;
   console.log('slug', slug);
-
 
   const initialPage = 1;
   const initialQuery = '';
@@ -17,5 +15,5 @@ export default async function NotesPage({params}: Props) {
 
   const initialData = await fetchNotes(initialPage, initialQuery, initialTag);
 
-  return <NotesClient initialData={initialData} initialTag={ initialTag} />;
+  return <NotesClient initialData={initialData} initialTag={initialTag} />;
 }
